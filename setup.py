90dc7a1386980069ec26e3fec623ffb166b163dd
Tomasz Habiger <thabiger@trans.eu>
2020-06-29 22:43:56 +0200

    lowers the required python version

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+import os
 from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='tha-gnome-terminator-tools',
-      version='0.1',
+      version="0.3.0",
       description='Automation scripts for Gnome Terminator',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -21,7 +22,7 @@ setup(name='tha-gnome-terminator-tools',
       scripts=['tha-gnome-terminator-tools/bin/terminator-ec2-ssh'],
       include_package_data=True,
       zip_safe=False,
-      python_requires='>=3.8',
+      python_requires='>=3.6',
       classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Other/Proprietary License",
