9a3d5727ca41506667135ab1a527420d20794ff6
Tomasz Habiger <thabiger@trans.eu>
2020-06-29 22:29:19 +0200

    test

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='tha-gnome-terminator-tools',
       scripts=['tha-gnome-terminator-tools/bin/terminator-ec2-ssh'],
       include_package_data=True,
       zip_safe=False,
-      python_requires='>=3.8',
+      python_requires='>=3.6',
       classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Other/Proprietary License",
