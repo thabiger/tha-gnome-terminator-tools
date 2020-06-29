1ff59f3e147182be67b99282cdd54aea74a78042
Tomasz Habiger <thabiger@trans.eu>
2020-06-29 22:31:35 +0200

    test

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+import os
 from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='tha-gnome-terminator-tools',
-      version='0.1',
+      version=os.getenv('CIRCLE_TAG'),
       description='Automation scripts for Gnome Terminator',
       long_description=long_description,
       long_description_content_type="text/markdown",
